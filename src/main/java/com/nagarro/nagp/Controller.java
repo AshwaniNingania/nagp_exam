7dcc4affbfd09ca4f9a76617f9eccd49305a99ef
nagarroPoS <ashwani01@nagarro.com>
2020-02-06 16:25:03 +0530

    Method rename

--- src/main/java/com/nagarro/nagp/Controller.java
+++ src/main/java/com/nagarro/nagp/Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     @GetMapping("/")
-    public String HelloWorld() {
+    public String helloWorld() {
         return "Hello World";
     }
 }
